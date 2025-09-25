b170d80b817474be446778a7d82286eedb523c48
Brekel <kaekaka7@gmail.com>
2025-09-25 20:42:53 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 <body>
     <h2>belajar repo remote 2</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam corrupti ab nulla nisi minus aspernatur et iure. Autem eos obcaecati sit aperiam expedita, totam voluptatibus tempora, consectetur maiores eum iusto.</p>
+    <li>satu</li>
+    <li>dua</li>
 </body>
 </html>
